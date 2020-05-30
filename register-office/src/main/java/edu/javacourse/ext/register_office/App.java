0d665a8964e6c32ec942bbe98d0a1571e1c970de
Dima Zosimov <d.zosimov94@mail.ru>
2020-05-30 16:21:54 +0300

    Register office init

---
+++ register-office/src/main/java/edu/javacourse/ext/register_office/App.java
@@ -0,0 +1,13 @@
+package edu.javacourse.ext.register_office;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
